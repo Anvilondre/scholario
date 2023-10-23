e82cf5b25ca0c22da0c3f395bf58559685067d39
Roman Burakov <32177478+Anvilondre@users.noreply.github.com>
2023-10-23 18:34:06 -0400

    Add cuda/cpu support with profiling (#1)
    Co-authored-by: Roman Burakov <burakov.r@gmail.com>

--- encoder_worker/encoder.py
+++ encoder_worker/encoder.py
@@ -1,4 +1,3 @@
-import os
 from sentence_transformers import SentenceTransformer
 
 class Encoder:
@@ -9,16 +8,8 @@ class Encoder:
             return cls._instance
         cls._instance = super(Encoder, cls).__new__(cls)
 
-        device = os.environ['DEVICE']
-
-        if device == 'auto':
-            import torch
-            if torch.cuda.is_available():
-                device = 'cuda'
-            elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
-                device = 'mps'
-            else:
-                device = 'cpu'
+        import torch
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         cls.model = SentenceTransformer('all-mpnet-base-v2').to(device)
 
