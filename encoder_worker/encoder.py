fbc04887bba84dbef5063f5579ba7d07d1af2610
Roman Burakov <buracov.r@gmail.com>
2023-10-23 11:24:56 -0400

    add base cuda image

--- encoder_worker/encoder.py
+++ encoder_worker/encoder.py
@@ -9,18 +9,7 @@ class Encoder:
             return cls._instance
         cls._instance = super(Encoder, cls).__new__(cls)
 
-        device = os.environ['DEVICE']
-
-        if device == 'auto':
-            import torch
-            if torch.cuda.is_available():
-                device = 'cuda'
-            elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
-                device = 'mps'
-            else:
-                device = 'cpu'
-
-        cls.model = SentenceTransformer('all-mpnet-base-v2').to(device)
+        cls.model = SentenceTransformer('all-mpnet-base-v2').to(os.environ['DEVICE'])
 
         return cls._instance
 
